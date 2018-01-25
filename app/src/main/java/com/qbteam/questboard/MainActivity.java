6597783ab89e38dea7fe34d83ca838e899097389
Sal Azmi <sazmi001@ucr.edu>
2018-01-25 12:57:11 -0800

    added button variables in java to corresponding buttons on mainactivity xml

--- app/src/main/java/com/qbteam/questboard/MainActivity.java
+++ app/src/main/java/com/qbteam/questboard/MainActivity.java
@@ -2,12 +2,20 @@ package com.qbteam.questboard;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button loginButton, joinButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginButton = (Button) findViewById(R.id.signinButton);
+        joinButton = (Button) findViewById(R.id.signupButton);
+
+
     }
 }
