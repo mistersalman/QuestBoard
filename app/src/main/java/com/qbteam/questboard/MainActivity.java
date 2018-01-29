1b3abdd65c540a276acd635a354b959fde81b373
Sal Azmi <sazmi001@ucr.edu>
2018-01-28 20:35:06 -0800

    Sign up button goes to acctcreation activity

--- app/src/main/java/com/qbteam/questboard/MainActivity.java
+++ app/src/main/java/com/qbteam/questboard/MainActivity.java
@@ -1,7 +1,9 @@
 package com.qbteam.questboard;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +18,15 @@ public class MainActivity extends AppCompatActivity {
         loginButton = (Button) findViewById(R.id.signinButton);
         joinButton = (Button) findViewById(R.id.signupButton);
 
+        joinButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent createIntent = new Intent(MainActivity.this,
+                        AcctCreation.class);
+                startActivity(createIntent);
+                //This is a simple construct used to transition from one activity to another
+            }
+        });
 
     }
 }
