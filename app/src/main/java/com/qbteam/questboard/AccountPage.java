ae9db9c4fefb6d3bcdefc1017e4ef65ee1ea01c7
Mark Spencer <mspen002@ucr.edu>
2018-02-04 11:02:33 -0800

    Created the account page template

--- app/src/main/java/com/qbteam/questboard/AccountPage.java
+++ app/src/main/java/com/qbteam/questboard/AccountPage.java
@@ -2,12 +2,25 @@ package com.qbteam.questboard;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AccountPage extends AppCompatActivity {
+
+    EditText bio;
+    Button changePicture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_page);
+
+        bio = (EditText) findViewById(R.id.bio);
+
+        changePicture = (Button) findViewById(R.id.changePicture);
+
+
+
     }
+
 }
