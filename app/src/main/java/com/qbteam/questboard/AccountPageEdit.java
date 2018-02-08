ccdcaacf31ac0839be02779e198e416ba8a55517
Mark Spencer <mspen002@ucr.edu>
2018-02-07 19:28:24 -0800

    Added edit version of the account page

---
+++ app/src/main/java/com/qbteam/questboard/AccountPageEdit.java
@@ -0,0 +1,161 @@
+package com.qbteam.questboard;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.firebase.ui.storage.images.FirebaseImageLoader;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+
+public class AccountPageEdit extends AppCompatActivity {
+
+    private static final int IMAGE_REQUEST_CODE = 22;
+    private static final int PDF_REQUEST_CODE = 23;
+    StorageReference storageRef = FirebaseStorage.getInstance().getReference();
+    EditText bio;
+    Button changePicture, uploadResume;
+    ImageView imageView;
+
+    Uri filePath, downloadUrl;
+    FirebaseAuth mobileAuth;
+    FirebaseUser currentUser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_page);
+
+        bio = (EditText) findViewById(R.id.bio);
+
+        changePicture = (Button) findViewById(R.id.changePicture);
+        uploadResume = (Button) findViewById(R.id.uploadResume);
+
+        mobileAuth = FirebaseAuth.getInstance();
+        currentUser = mobileAuth.getCurrentUser();
+
+        StorageReference pathRef = storageRef.child(currentUser.getEmail()+"/profile.png");
+        imageView = (ImageView) findViewById(R.id.imageView);
+        Glide.with(this)
+                .using(new FirebaseImageLoader())
+                .load(pathRef)
+                .diskCacheStrategy(DiskCacheStrategy.NONE)
+                .skipMemoryCache(true)
+                .into(imageView);
+
+
+        changePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Opens file explorer and looks for an image
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                //Calls onActivityResult after exit
+                startActivityForResult(intent, IMAGE_REQUEST_CODE);
+
+            }
+        });
+
+        uploadResume.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Opens file explorer and looks for a pdf
+                Intent intent = new Intent();
+                intent.setType("application/pdf"); //for pdf
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                //Calls onActivityResult after exit
+                startActivityForResult(intent, PDF_REQUEST_CODE);
+
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //Get current user
+        mobileAuth = FirebaseAuth.getInstance();
+        currentUser = mobileAuth.getCurrentUser();
+
+        //Gets and loads current user's profile.png into imageview
+        StorageReference pathRef = storageRef.child(currentUser.getEmail()+"/profile.png");
+        imageView = (ImageView) findViewById(R.id.imageView);
+        Glide.with(this)
+                .using(new FirebaseImageLoader())
+                .load(pathRef)
+                .diskCacheStrategy(DiskCacheStrategy.NONE)
+                .skipMemoryCache(true)
+                .into(imageView);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //Check if image is selected
+        if(requestCode == IMAGE_REQUEST_CODE && resultCode == RESULT_OK && data != null && data.getData() != null)
+        {
+            //Uploads image as profile.png, works for jpg as well
+            //Gets path of image
+            filePath = data.getData();
+            //Set destination
+            StorageReference fileRef = storageRef.child(currentUser.getEmail()+"/profile.png");
+            //Uploads the file
+            UploadTask uploadTask = fileRef.putFile(filePath);
+            //Runs depending on upload success or failure, current ain't doing anything
+            uploadTask.addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    // Handle unsuccessful uploads
+                }
+            }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    // taskSnapshot.getMetadata() contains file metadata such as size, content-type, and download URL.
+                    downloadUrl = taskSnapshot.getDownloadUrl();
+                }
+            });
+
+        }
+
+        if(requestCode == PDF_REQUEST_CODE && resultCode == RESULT_OK && data != null && data.getData() != null)
+        {
+            //Uploads image as profile.png, works for jpg as well
+            //Gets path of image
+            filePath = data.getData();
+            //Set destination
+            StorageReference fileRef = storageRef.child(currentUser.getEmail()+"/resume.pdf");
+            //Uploads the file
+            UploadTask uploadTask = fileRef.putFile(filePath);
+            //Runs depending on upload success or failure, current ain't doing anything
+            uploadTask.addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    // Handle unsuccessful uploads
+                }
+            }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    // taskSnapshot.getMetadata() contains file metadata such as size, content-type, and download URL.
+                    downloadUrl = taskSnapshot.getDownloadUrl();
+                }
+            });
+
+        }
+    }
+
+}
