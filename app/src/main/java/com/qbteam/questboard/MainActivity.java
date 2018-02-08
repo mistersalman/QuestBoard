d8af9c799728c0b49e87b3fe9a3b0b9b4ebe3b34
Sal Azmi <sazmi001@ucr.edu>
2018-02-07 21:39:54 -0800

    fixed build error

--- app/src/main/java/com/qbteam/questboard/MainActivity.java
+++ app/src/main/java/com/qbteam/questboard/MainActivity.java
@@ -9,7 +9,7 @@ import android.widget.Button;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class MainActivity extends AppCompatActivity implements QBUser{
+public class MainActivity extends AppCompatActivity {
 
     private Button acctMgmt, logout;
     private FirebaseAuth mobileAuth;
