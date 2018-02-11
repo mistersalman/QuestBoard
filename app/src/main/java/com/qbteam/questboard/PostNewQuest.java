0f2288cd752c6ab33fdea11da2cb6a2d9757486d
Robyn Haley <rhale002@student.ucr.edu>
2018-02-10 21:27:51 -0800

    Added activities for posting a new quest and for editing a posted quest

---
+++ app/src/main/java/com/qbteam/questboard/PostNewQuest.java
@@ -0,0 +1,13 @@
+package com.qbteam.questboard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PostNewQuest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_new_quest);
+    }
+}
