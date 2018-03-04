d200eddae045f79d260ff6eed736d8764150ef12
Robyn Haley <rhale002@student.ucr.edu>
2018-03-03 16:58:52 -0800

    Added view applicants activity

---
+++ app/src/main/java/com/qbteam/questboard/ViewQuestApplicants.java
@@ -0,0 +1,13 @@
+package com.qbteam.questboard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewQuestApplicants extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_quest_applicants);
+    }
+}
