c0f4d99f1c9b878659803f20f8f310e363fe0f72
Sal Azmi <sazmi001@ucr.edu>
2018-03-07 02:51:14 -0800

    Succesfully retrieving address of corresponding coordinates. Need front-end to show address and distance.
    functional as of second demo (2/22) need to check parseInt (AccountPageEdit:116)

--- app/src/main/java/com/qbteam/questboard/PostNewQuest.java
+++ app/src/main/java/com/qbteam/questboard/PostNewQuest.java
@@ -45,6 +45,7 @@ public class PostNewQuest extends AppCompatActivity implements LocationListener{
     FirebaseUser currentUser;
     double latitude = 0;
     double longitude = 0;
+    String address = "";
     LocationManager locationManager;
 
     @Override
@@ -178,6 +179,8 @@ public class PostNewQuest extends AppCompatActivity implements LocationListener{
             Log.d("address", addresses.get(0).getAddressLine(0) + addresses.get(0).getAddressLine(1) + addresses.get(0).getAddressLine(2));
             latitude = location.getLatitude();
             longitude = location.getLongitude();
+            address = addresses.get(0).getAddressLine(0) + ", " + addresses.get(0).getAddressLine(1) + ", " + addresses.get(0).getAddressLine(2);
+            Log.d("address:", address);
             Log.d("stored latitude", Double.toString(latitude));
             Log.d("stored longitude", Double.toString(longitude));
         }
